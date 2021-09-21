44b425493627d2ddb3ef44ea728e9f138a124d30
dilyana2001 <dilyana2001@gmail.com>
2021-09-21 15:14:32 +0300

    Adding Header, HomePage and Footer components

---
+++ client/src/components/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+const HomePage = () => {
+
+    return (
+        <main className="container">
+            <section>
+                <h1>POSTS</h1>
+
+            </section>
+
+        </main>
+    );
+}
+
+
+export default HomePage;
