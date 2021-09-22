8bfa3414762b41456759e0d77d84b1b64c035d30
dilyana2001 <dilyana2001@gmail.com>
2021-09-22 21:42:10 +0300

    header navigation authenticate

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,31 +5,39 @@ import auth from '../../Service/auth';
 import './Header.css';
 
 const Header = () => {
+
     const username = localStorage.getItem('username');
+    const token = localStorage.getItem('auth_token')
+
+    const navbarLoggedUser =
+        <ul className="navbar-logged-user">
+            <li className="first-bar">
+                <NavLink className="button" to='/'>Home</NavLink>
+                <NavLink className="button" to="/create-post">Create Post</NavLink>
+                <NavLink className="button" to={`/profile`}>Profile</NavLink>
+            </li>
+            <li className="second-bar">
+                <ul>
+                    <li className="welcome-header">Welcome, {username}</li>
+                    <li><NavLink to="#" onClick={() => {
+                        auth.logout();
+                    }}> Logout</NavLink></li>
+                </ul>
+            </li>
+        </ul>;
+
+    const navbarAnonymous =
+        <ul className="navbar-anonymous">
+            <li>
+                <NavLink to="/register">Register</NavLink>
+                <NavLink to="/login">Login</NavLink>
+            </li>
+        </ul>;
+
     return (
         <header className="site-header">
             <nav className="navbar">
-                <ul className="navbar-logged-user">
-                    <li className="first-bar">
-                        <NavLink className="button" to='/'>Home</NavLink>
-                        <NavLink className="button" to="/create-post">Create Post</NavLink>
-                        <NavLink className="button" to={`/profile`}>Profile</NavLink>
-                    </li>
-                    <li className="second-bar">
-                        <ul>
-                            <li className="welcome-header">Welcome, {username}</li>
-                            <li><NavLink to="#" onClick={() => {
-                                auth.logout();
-                            }}> Logout</NavLink></li>
-                        </ul>
-                    </li>
-                </ul>
-                <ul className="navbar-anonymous">
-                    <li>
-                        <NavLink to="/register">Register</NavLink>
-                        <NavLink to="/login">Login</NavLink>
-                    </li>
-                </ul>
+                {(token != null) ? navbarLoggedUser : navbarAnonymous}
             </nav>
         </header>
     );
