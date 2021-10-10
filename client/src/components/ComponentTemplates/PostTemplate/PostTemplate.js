135d8751a7863a7515866061c17085f2ccd042ce
dilyana2001 <dilyana2001@gmail.com>
2021-10-10 18:21:18 +0300

    profile image added to posts

--- client/src/components/ComponentTemplates/PostTemplate/PostTemplate.js
+++ client/src/components/ComponentTemplates/PostTemplate/PostTemplate.js
@@ -2,13 +2,13 @@ import { NavLink } from "react-router-dom";
 
 import './PostTemplate.css';
 
-const PostTemplate = ({ data }) => {
+const PostTemplate = ({ data, profile }) => {
     return (
         <li className="postTemplate">
             <div className="postTemplate-container">
                 <div className="user-info">
-                <NavLink to={`/profile/${data.userId}`}>  <img className="profile-image"
-                     src="https://cdn3.vectorstock.com/i/thumb-large/53/52/person-private-userpic-business-character-profile-vector-23565352.jpg" /></NavLink>
+                    <NavLink to={`/profiles/${data.userId}`}>  <img className="profile-image"
+                        src={profile?.imageUrl || 'https://cdn3.vectorstock.com/i/thumb-large/53/52/person-private-userpic-business-character-profile-vector-23565352.jpg'} /></NavLink>
                     <p className="username-paragraph">{data.username} post:</p>
                 </div>
                 <div className="meme-info">
@@ -17,7 +17,6 @@ const PostTemplate = ({ data }) => {
                     <div className="post-info">
                     </div>
                 </div>
-
             </div>
         </li>
     );
