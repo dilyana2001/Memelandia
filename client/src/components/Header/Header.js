4564fb7b72323afa383ebae14e0c10ede8580547
dilyana2001 <dilyana2001@gmail.com>
2021-10-11 15:32:00 +0300

    add search bar and apply css

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,5 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
 import auth from '../../Service/auth';
 
@@ -10,22 +11,29 @@ const Header = () => {
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
 
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        auth.getMyMessages(userId)
+            .then(setMessages)
+    }, [])
+
     const navbarLoggedUser =
         <ul className="navbar-logged-user">
             <li className="first-bar">
                 <NavLink className="button" to='/' exact >Home</NavLink>
-                <NavLink className="button" to="/create-post">Create Post</NavLink>
-                <NavLink className="button" to={`/profiles/${userId}`}>Profile</NavLink>
+                <Link className="button add-post" to="/create-post"><i class="fas fa-plus"></i></Link>
                 <NavLink className="button" to={`/friends`}>Find Friends</NavLink>
                 <NavLink className="button" to={`/about`}>About</NavLink>
-                <NavLink className="button" to={`/messages/${userId}`}>1</NavLink>
             </li>
             <li className="second-bar">
                 <ul>
-                    <li className="welcome-header">Welcome, {username}</li>
-                    <li><NavLink to="#" onClick={() => {
+                    <li className="welcome-header">Welcome,</li>
+                    <li><NavLink className="button" to={`/profiles/${userId}`}>{username}</NavLink></li>
+                    <li><NavLink className="button" to={`/messages/${userId}`}>{messages.length}</NavLink></li>
+                    <li><Link className="button logout" to="#" onClick={() => {
                         auth.logout();
-                    }}> Logout</NavLink></li>
+                    }}> Logout</Link></li>
                 </ul>
             </li>
         </ul>;
@@ -33,7 +41,7 @@ const Header = () => {
     const navbarAnonymous =
         <ul className="navbar-anonymous">
             <li>
-            <NavLink className="button" to='/' exact >Home</NavLink>
+                <NavLink className="button" to='/' exact >Home</NavLink>
                 <NavLink to="/register">Register</NavLink>
                 <NavLink to="/login">Login</NavLink>
             </li>
--- client/src/components/MainPages/About/About.js
+++ client/src/components/MainPages/About/About.js
@@ -1,12 +1,18 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
+
+import './About.css'
 
 class About extends Component {
     render() {
         return (
             <div className="main-container">
                 <section className="about-section">
-                    <h2>MEMELANDIA</h2>
-                    <p>inspired by facebook</p>
+                    <h1>MEMELANDIA</h1>
+                    <p>Inspired by Facebook!</p>
+                    <Link to="#"><p>Contact us!</p></Link>  
+                    <p>You can add post from the big pink plus! </p>
+                        <p>Have fun!</p>
                 </section>
             </div>
         )
