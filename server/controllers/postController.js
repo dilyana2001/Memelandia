4419982bc5608f120e68c34d016b59e1a8d2cba5
dilyana2001 <dilyana2001@gmail.com>
2021-11-19 22:52:07 +0200

    fix catches in api

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -6,65 +6,74 @@ const { isAuth } = require('../middlewares/auth');
 router.get('/', (req, res) => {
     Post.find().sort({ '_id': -1 })
         .then(posts => res.json(posts))
-        .catch(() => next({ status: 404, message: 'Not Found!', type: 'ERROR' }));
+        .catch(() => res.status(404).json({ message: 'Not Found!', type: 'ERROR' }));
 });
 
 router.get('/:postId', (req, res) => {
     Post.findById(req.params.postId)
         .then(post => res.json(post))
-        .catch(() => next({ status: 404, message: 'Not Found!', type: 'ERROR' }));
+        .catch(() => res.status(404).json({ message: 'Not Found!', type: 'ERROR' }));
+
 });
 
 router.post('/', isAuth, (req, res) => {
     let post = new Post(req.body);
     post.save()
         .then(createdPost => res.status(201).json({ _id: createdPost._id }))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
+
 });
 
 router.delete('/:postId', isAuth, (req, res) => {
     Post.findByIdAndRemove(req.params.postId)
         .then(deleted => res.json({ _id: deleted._id }))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
+
 });
 
 router.put('/:postId', isAuth, (req, res) => {
     const post = new Post({ _id: req.params.postId, ...req.body });
     Post.findByIdAndUpdate(req.params.postId, post)
         .then(updated => res.status(201).json({ _id: updated._id }))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
+
 });
 
 router.post('/likes', isAuth, (req, res) => {
     let like = new Like(req.body);
     like.save()
         .then(createdLike => res.status(201).json({ _id: createdLike._id }))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
+
 });
 
 router.delete('/likes/postId/:postId/userId/:userId', isAuth, (req, res) => {
     Like.findOneAndDelete({ postId: req.params.postId, userId: req.params.userId })
         .then(deleted => res.json({ _id: deleted._id }))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
+
 });
 
 router.get('/userId/:id', (req, res) => {
     Post.find({ userId: req.params.id }).sort({ '_id': -1 })
         .then(posts => res.json(posts))
-        .catch(() => next({ status: 404, message: 'Not Found!', type: 'ERROR' }));
+        .catch(() => res.status(404).json({ message: 'Not Found!', type: 'ERROR' }));
+
 });
 
 router.get('/likes/:id', (req, res) => {
     Like.find({ postId: req.params.id })
         .then(likes => res.json(likes))
-        .catch(() => next({ status: 404, message: 'Not Found!', type: 'ERROR' }));
+        .catch(() => res.status(404).json({ message: 'Not Found!', type: 'ERROR' }));
+
 });
 
 
 router.get('/likes/postId/:postId/userId/:userId', (req, res) => {
     Like.findOne({ postId: req.params.postId, userId: req.params.userId })
         .then(likes => res.json(likes))
-        .catch(() => next({ status: 404, message: 'Not Found!', type: 'ERROR' }));
+        .catch(() => res.status(404).json({ message: 'Not Found!', type: 'ERROR' }));
+
 });
 
 module.exports = router;
--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -6,7 +6,7 @@ const { isAuth } = require('../middlewares/auth');
 router.get('/:id', (req, res) => {
     Profile.findOne({ userId: req.params.id })
         .then(profile => res.json(profile))
-        .catch(() => next({ status: 404, message: 'Not Found!', type: 'ERROR' }));
+        .catch(() => res.status(404).json({ message: 'Not Found!', type: 'ERROR' }));
 });
 
 router.post('/:id', isAuth, (req, res) => {
@@ -14,38 +14,38 @@ router.post('/:id', isAuth, (req, res) => {
 
     profile.save()
         .then(createdProfile => res.status(201).json({ _id: createdProfile._id }))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
 });
 
 router.put('/:id', isAuth, (req, res) => {
     const profile = new Profile({ _id: req.params.id, ...req.body });
     Profile.findByIdAndUpdate(req.params.id, profile)
         .then(updated => res.status(201).json({ _id: updated._id }))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
 });
 
 router.get('/', isAuth, (req, res) => {
     Profile.find().sort({ 'username': 1 })
         .then(profiles => res.json(profiles))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
 });
 
 router.get('/search/:query', (req, res) => {
     Profile.find({ username: { $regex: `${req.params.query}`, $options: "i" } }).sort({ 'username': 1 })
         .then(profiles => res.json(profiles))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
 });
 
 router.delete('/:userId', isAuth, (req, res) => {
     Profile.findOneAndRemove({ userId: req.params.userId })
         .then(deleted => res.status(200).json({ _id: deleted._id }))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
 });
 
 router.delete('/users/:id', isAuth, (req, res) => {
     User.findOneAndRemove({ _id: req.params.id })
         .then(deleted => res.status(200).json({ _id: deleted._id }))
-        .catch(() => next({ status: 403, message: 'Forbidden!', type: 'ERROR' }));
+        .catch(() => res.status(403).json({ message: 'Forbidden!', type: 'ERROR' }));
 });
 
 module.exports = router;
